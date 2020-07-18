fdf03f9694bf0c31727720ada2961e429c540a88
Justin Stewart <stewartjustin3@gmail.com>
2020-07-18 14:49:03 -0400

    setup authed users

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -2,7 +2,7 @@ import { RECEIVE_TWEETS } from '../actions/tweets';
 
 export default function users(state = {}, action) {
   switch (action.type) {
-    case RECEIVE_USERS:
+    case RECEIVE_TWEETS:
       return {
         ...state,
         ...action.users,
