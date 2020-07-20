7ea168dad1080e6fcede2badf33f5db9da94d7b0
Justin Stewart <stewartjustin3@gmail.com>
2020-07-20 11:03:27 -0400

    added feature so tweets push to console

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
+import Dashboard from './Dashboard'
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
   render() {
-    return <div>Starter Code</div>;
+    return( <Dashboard/>)
+   
   }
 }
 
